fbb62c8bb1a2a307ace6d782144e905baff2bb05
F74041022 <F74041022@mail.ncku.edu.tw>
2018-01-03 14:40:28 +0800

    chatbot

---
+++ app.py
@@ -0,0 +1,213 @@
+import sys
+from io import BytesIO
+
+import telegram
+from flask import Flask, request, send_file
+
+from fsm import TocMachine
+
+
+API_TOKEN = '480513885:AAEwQboNf9fJUuE13pBQLl4URAj5F3QCT00'
+WEBHOOK_URL = 'https://83a55c17.ngrok.io/hook'
+
+app = Flask(__name__)
+bot = telegram.Bot(token=API_TOKEN)
+machine = TocMachine(
+    states=[
+        'initial',
+        'name',
+        'check',        
+        'normal',
+        
+        'hungry',
+        'hungry_option1',
+        'hungry_option2',        
+
+        'sick',
+        'sick_option1',
+        'sick_option2',        
+
+        'boring',
+        'boring_option1',
+        'boring_option2',
+        'boring_option3',        
+
+        'angry',
+        'angry_option1',
+        'angry_option2',
+        'angry_option3',
+        
+        'dead'
+    ],
+    transitions=[         
+        {
+            'trigger': 'advance',
+            'source': 'initial',
+            'dest': 'name',
+            'conditions': 'is_going_to_name'
+        }, 
+
+        {
+            'trigger': 'advance',
+            'source': 'dead',
+            'dest': 'initial',
+            'conditions': 'is_going_to_initial'
+        },        
+
+        {
+            'trigger': 'advance',
+            'source': 'boring',
+            'dest': 'boring_option1',
+            'conditions': 'is_going_to_boring_option1'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'boring',
+            'dest': 'boring_option2',
+            'conditions': 'is_going_to_boring_option2'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'boring',
+            'dest': 'boring_option3',
+            'conditions': 'is_going_to_boring_option3'
+        },
+
+        {
+            'trigger': 'advance',
+            'source': 'angry',
+            'dest': 'angry_option1',
+            'conditions': 'is_going_to_angry_option1'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'angry',
+            'dest': 'angry_option2',
+            'conditions': 'is_going_to_angry_option2'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'angry',
+            'dest': 'angry_option3',
+            'conditions': 'is_going_to_angry_option3'
+        },
+
+        {
+            'trigger': 'advance',
+            'source': 'hungry',
+            'dest': 'hungry_option1',
+            'conditions': 'is_going_to_hungry_option1'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'hungry',
+            'dest': 'hungry_option2',
+            'conditions': 'is_going_to_hungry_option2'
+        },
+
+        {
+            'trigger': 'advance',
+            'source': 'sick',
+            'dest': 'sick_option1',
+            'conditions': 'is_going_to_sick_option1'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'sick',
+            'dest': 'sick_option2',
+            'conditions': 'is_going_to_sick_option2'
+        },
+        {
+            'trigger': 'advance',
+            'source' : ['hungry',
+                        'angry',
+                        'sick',
+                        'boring'],
+            'dest': 'normal',
+            'conditions': 'do_nothing'
+        },
+
+        {
+            'trigger': 'is_going_to_check',
+            'source': 'normal',
+            'dest': 'check',            
+        },
+
+        {
+            'trigger': 'is_going_to_normal',
+            'source' : ['name',
+                        'check',
+                        'boring_option1',
+                        'boring_option2',
+                        'boring_option3',
+                        'angry_option1',
+                        'angry_option2',
+                        'angry_option3',
+                        'hungry_option1',
+                        'hungry_option2',
+                        'sick_option1',
+                        'sick_option2'],
+            'dest': 'normal',             
+        },
+        {
+            'trigger': 'is_going_to_hungry',
+            'source': 'normal',
+            'dest': 'hungry',             
+        },
+        {
+            'trigger': 'is_going_to_sick',
+            'source' : ['normal',
+                        'angry'],
+            'dest': 'sick',             
+        },
+        {
+            'trigger': 'is_going_to_angry',
+            'source' : ['normal',
+                        'boring'],
+            'dest': 'angry',             
+        },
+        {
+            'trigger': 'is_going_to_boring',
+            'source': 'normal',
+            'dest': 'boring',             
+        },
+        {
+            'trigger': 'is_going_to_dead',
+            'source' : ['hungry',
+                        'sick'],
+            'dest': 'dead',             
+        }
+    ],
+    initial='initial',
+    auto_transitions=False,
+    show_conditions=True,
+)
+
+
+def _set_webhook():
+    status = bot.set_webhook(WEBHOOK_URL)
+    if not status:
+        print('Webhook setup failed')
+        sys.exit(1)
+    else:
+        print('Your webhook URL has been set to "{}"'.format(WEBHOOK_URL))
+
+
+@app.route('/hook', methods=['POST'])
+def webhook_handler():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    machine.advance(update)
+    return 'ok'
+
+
+@app.route('/show-fsm', methods=['GET'])
+def show_fsm():
+    byte_io = BytesIO()
+    machine.graph.draw(byte_io, prog='dot', format='png')
+    byte_io.seek(0)
+    return send_file(byte_io, attachment_filename='fsm.png', mimetype='image/png')
+
+
+if __name__ == "__main__":
+    _set_webhook()
+    app.run()
---
+++ fsm.py
@@ -0,0 +1,343 @@
+from transitions.extensions import GraphMachine
+import time
+
+life_level = 100
+sick_level = 0
+mood_level = 0
+boring_level = 0
+hungry_level = 0
+money = 100   
+name = 'Joseph'
+
+class TocMachine(GraphMachine):
+    def __init__(self, **machine_configs):
+        self.machine = GraphMachine(
+            model = self,
+            **machine_configs
+        )  
+    """initial ok"""
+    def is_going_to_initial(self, update):
+        text = update.message.text                        
+        return text.lower() == 'initial'
+
+    def on_enter_initial(self, update):        
+        update.message.reply_text("Initial and 'Intro'")
+        global life_level, sick_level, mood_level, boring_level, hungry_level, money        
+        life_level = 100
+        sick_level = 0
+        mood_level = 0
+        boring_level = 0
+        hungry_level = 0
+        money = 100           
+
+    def on_exit_initial(self, update):
+        print('exit initial')
+
+    """name ok"""
+    def is_going_to_name(self, update):
+        text = update.message.text                        
+        return text.lower() == 'intro'
+
+    def on_enter_name(self, update):        
+        update.message.reply_text('Hi! My name is ' + name) 
+        self.is_going_to_normal(update)       
+
+    def on_exit_name(self, update):
+        update.message.reply_text('Hungry Level : ' +str(hungry_level)
+                                + '\nSick Level : ' +str(sick_level)
+                                + '\nMood Level : ' +str(mood_level)
+                                + '\nBoring Level : ' +str(boring_level)
+                                + '\nMoney : '+str(money))
+        print('exit name')
+
+    """normal"""
+    def is_going_to_normal(self, update):               
+        print('go to normal')
+
+    def on_enter_normal(self, update):
+        print('normal') 
+        global life_level, sick_level, mood_level, boring_level, hungry_level, money
+        counter = 0      
+        update.message.reply_text(' Life Level : ' + str(life_level))
+        while(counter<5):            
+            time.sleep(30)
+
+            money = money + 30
+            sick_level = sick_level + 3
+            mood_level = mood_level + 4
+            boring_level = boring_level + 5
+            hungry_level = hungry_level + 8                                 
+
+            if(sick_level>80 or mood_level>80):
+                self.is_going_to_sick(update)
+                break 
+
+            if(hungry_level>70):               
+                self.is_going_to_hungry(update)
+                break
+
+            if(mood_level>50 or boring_level>50):
+                self.is_going_to_angry(update)
+                break            
+
+            if(boring_level>20):               
+                self.is_going_to_boring(update)
+                break                        
+
+            if(life_level<90):
+                life_level = life_level + 10
+            
+            counter = counter+1
+        
+        self.is_going_to_check(update)
+                            
+
+    def on_exit_normal(self, update):
+        print('exit normal')
+    
+    
+    """check ok"""
+    def is_going_to_check(self, update):
+        print('go to check')
+
+    def on_enter_check(self, update):        
+        update.message.reply_text('Name : ' + name
+                                + '\nLife Level : ' +str(life_level)
+                                + '\nHungry Level : ' +str(hungry_level)
+                                + '\nSick Level : ' +str(sick_level)
+                                + '\nMood Level : ' +str(mood_level)
+                                + '\nBoring Level : ' +str(boring_level)
+                                + '\nMoney : '+str(money))
+        self.is_going_to_normal(update)
+
+    def on_exit_check(self, update):
+        print('exit check')
+
+    """hungry"""
+    def is_going_to_hungry(self, update):        
+        print('go to hungry')
+
+    def on_enter_hungry(self, update):               
+        update.message.reply_text("I'm hungry.\n\noption[H1] -30 $20\noption[H2] -10 $10\nHungry Level : "+str(hungry_level)+"\nMoney : " + str(money)+'\n\nor do nothing')      
+        global life_level
+        life_level = life_level - 10
+        if(life_level<0):
+            self.is_going_to_dead(dead)
+    
+    def on_exit_hungry(self, update):       
+        print('exit hungry')
+
+    """hungry option1"""
+    def is_going_to_hungry_option1(self, update):
+        text = update.message.text        
+        return text.lower() == 'h1'
+
+    def on_enter_hungry_option1(self, update):               
+        update.message.reply_text('option[H1] -30 $20') 
+        global hungry_level, money
+        hungry_level = hungry_level -30
+        money = money-20       
+        self.is_going_to_normal(update)       
+
+    def on_exit_hungry_option1(self, update): 
+        update.message.reply_text('Fed')      
+        print('exit hungry option1')
+        
+    """hungry option2"""
+    def is_going_to_hungry_option2(self, update):
+        text = update.message.text        
+        return text.lower() == 'h2'
+
+    def on_enter_hungry_option2(self, update):               
+        update.message.reply_text('option[H2] -10 $10')
+        global hungry_level, money
+        hungry_level = hungry_level -10
+        money = money-10        
+        self.is_going_to_normal(update)
+
+    def on_exit_hungry_option2(self, update):
+        update.message.reply_text('Fed')       
+        print('exit hungry option2')  
+        
+
+    """sick"""
+    def is_going_to_sick(self, update):
+        print('go to sick')
+
+    def on_enter_sick(self, update):        
+        update.message.reply_text("I'm sick.\n\noption[S1] -50 $30\noption[S2] -80 $60\nSick Level : "+str(sick_level)+"\nMoney : " + str(money)+'\n\nor do nothing')        
+        global life_level
+        life_level = life_level - 20
+        if(life_level<0):
+            self.is_going_to_dead(update)
+    
+    def on_exit_sick(self, update):
+        print('exit sick')
+
+    """sick option1"""
+    def is_going_to_sick_option1(self, update):
+        text = update.message.text        
+        return text.lower() == 's1'
+
+    def on_enter_sick_option1(self, update):        
+        update.message.reply_text("option[S1] -50 $30") 
+        global sick_level, money
+        sick_level = sick_level -50
+        money = money-30
+        self.is_going_to_normal(update)        
+
+    def on_exit_sick_option1(self, update):
+        update.message.reply_text('Cured')
+        print('exit sick option1')
+
+    """sick option2"""
+    def is_going_to_sick_option2(self, update):
+        text = update.message.text        
+        return text.lower() == 's2'
+
+    def on_enter_sick_option2(self, update):        
+        update.message.reply_text("option[S2] -80 $60") 
+        global sick_level, money
+        sick_level = sick_level -80
+        money = money-60
+        self.is_going_to_normal(update)        
+
+    def on_exit_sick_option2(self, update):
+        update.message.reply_text('Cured')
+        print('exit sick option2')
+
+    """boring"""
+    def is_going_to_boring(self, update):
+        print('go to boring')
+
+    def on_enter_boring(self, update):        
+        update.message.reply_text("I'm boring.\n\noption[B1] -30 $20\noption[B2] -20 $15\noption[B3] -10 $10\nBoring Level : "+str(boring_level)+"\nMoney : " + str(money)+'\n\nor do nothing')       
+        global life_level
+        life_level = life_level - 2        
+
+    def on_exit_boring(self, update):        
+        print('exit boring')
+
+    """boring option1"""
+    def is_going_to_boring_option1(self, update):
+        text = update.message.text        
+        return text.lower() == 'b1'
+
+    def on_enter_boring_option1(self, update):        
+        update.message.reply_text("option[B1] -30 $20")  
+        global boring_level, money
+        boring_level = boring_level -30
+        money = money-20      
+        self.is_going_to_normal(update)
+
+    def on_exit_boring_option1(self, update):
+        update.message.reply_text("Got somthing to do")        
+        print('exit boring option1')
+
+    """boring option2"""
+    def is_going_to_boring_option2(self, update):
+        text = update.message.text        
+        return text.lower() == 'b2'
+
+    def on_enter_boring_option2(self, update):        
+        update.message.reply_text("option[B2] -20 $15") 
+        global boring_level, money
+        boring_level = boring_level -20
+        money = money-15     
+        self.is_going_to_normal(update)
+
+    def on_exit_boring_option2(self, update): 
+        update.message.reply_text("Got somthing to do.")       
+        print('exit boring option2')
+
+    """boring option3"""
+    def is_going_to_boring_option3(self, update):
+        text = update.message.text        
+        return text.lower() == 'b3'
+
+    def on_enter_boring_option3(self, update):        
+        update.message.reply_text("option[B3] -10 $10")
+        global boring_level, money
+        boring_level = boring_level -10
+        money = money-10        
+        self.is_going_to_normal(update)
+
+    def on_exit_boring_option3(self, update):
+        update.message.reply_text("Got somthing to do.")        
+        print('exit boring option3')
+
+    """angry"""
+    def is_going_to_angry(self, update):
+        print('go to angry')
+
+    def on_enter_angry(self, update):         
+        update.message.reply_text("I'm angry.\n\noption[A1] -30 $40\noption[A2] -20 $20\noption[A3] -10 $10\nMood Level : "+str(mood_level)+"\nMoney : " + str(money)+'\n\nor do nothing')        
+        global life_level
+        life_level = life_level - 2        
+
+    def on_exit_angry(self, update):
+        print('exit angry')
+
+    """angry option1"""
+    def is_going_to_angry_option1(self, update):
+        text = update.message.text        
+        return text.lower() == 'a1'
+
+    def on_enter_angry_option1(self, update):         
+        update.message.reply_text("option[A1] -30 $40")
+        global mood_level, money
+        mood_level = mood_level -30
+        money = money-40        
+        self.is_going_to_normal(update)
+
+    def on_exit_angry_option1(self, update):
+        update.message.reply_text("I'm good.")
+        print('exit angry option1')
+
+    """angry option2"""
+    def is_going_to_angry_option2(self, update):
+        text = update.message.text        
+        return text.lower() == 'a2'
+
+    def on_enter_angry_option2(self, update):         
+        update.message.reply_text("option[A2] -20 $20") 
+        global mood_level, money
+        mood_level = mood_level -20
+        money = money-20       
+        self.is_going_to_normal(update)
+
+    def on_exit_angry_option2(self, update):
+        update.message.reply_text("I'm good.")
+        print('exit angry option2')
+
+    """angry option3"""
+    def is_going_to_angry_option3(self, update):
+        text = update.message.text        
+        return text.lower() == 'a3'
+
+    def on_enter_angry_option3(self, update):         
+        update.message.reply_text("option[A3] -10 $10")
+        global angry_level, money
+        mood_level = mood_level -10
+        money = money-10        
+        self.is_going_to_normal(update)
+
+    def on_exit_angry_option3(self, update):
+        update.message.reply_text("I'm good.")
+        print('exit angry option3')
+
+    """dead"""
+    def is_going_to_dead(self, update):
+        print("dead")
+
+    def on_enter_dead(self, update):        
+        update.message.reply_text("I'm dead. Wait for 10 minutes and initialize 'initial'.")
+        time.sleep(600)
+
+    def on_exit_dead(self, update):
+        update.message.reply_text('You can start again.')
+        print('Initial')
+
+    def do_nothing(self, update):
+        text = update.message.text        
+        return text.lower() == 'do nothing'
